1abfae1946520bbde72b4d4206f391f4a5ac6de5
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-09-14 23:21:47 +0200

    make counter

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 defineProps<{ title: string }>();
+const count = ref(0);
+
+const increment = () => (count.value += 1);
 </script>
 
 <template>
   <h2>{{ title }}</h2>
+  <button @click="increment">{{ count }}</button>
 </template>
