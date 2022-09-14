ac45e5b6bc0b4a6b24429f8c1280b567faac1932
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-09-14 22:51:11 +0200

    make counter

--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -1,1 +1,17 @@
-alert("hello worl");
+type Todo = {
+  id: string;
+  label: string;
+};
+
+let count = 0;
+
+const addTodoButton =
+  document.querySelector<HTMLButtonElement>("#add-todo-btn")!;
+const inputElement = document.querySelector<HTMLInputElement>("#todo-input")!;
+
+addTodoButton!.addEventListener("click", () => {
+  inputElement!.value = count.toString();
+  count += 1;
+});
+
+export {};
