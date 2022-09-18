aa1c2ec0a27b9444b0d709050caf2fa80098f793
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-09-18 15:29:27 +0200

    messing around with svelte

--- svelte/src/lib/Counter.svelte
+++ svelte/src/lib/Counter.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+  const decrement = () => {
+    count -= 1;
+  };
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+<button on:click={decrement}> decrement </button>
---
+++ svelte/src/lib/Todo.svelte
@@ -0,0 +1,1 @@
+<h2>hello worl</h2>
